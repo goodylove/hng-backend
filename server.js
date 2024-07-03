b507fe7fe065f3068ae428fea3f8c0ec0ee10098
goodness <goodyc474@gmail.com>
2024-07-03 21:28:42 +0100

    fix error

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const app = express();
 app.use(express.json());
 app.set('trust proxy', true);
 
+
 app.use("/api", routes)
+app.use("/*", (req, res) => {
+    res.send("Welcome Hng Internship")
+})
 
 
 const port = process.env.PORT || 3000
